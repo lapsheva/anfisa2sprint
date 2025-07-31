e5413c44f63ca360c005a4d0157a079b066f8623
Ëlena Lapsheva <lapsheva@gmail.com>
2025-07-31 16:29:14 +0400

    Add filter

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from django.db.models import Q
+
+from ice_cream.models import IceCream
 
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+    # Заключаем вызов методов в скобки
+    # (это стандартный способ переноса длинных строк в Python);
+    # каждый вызов пишем с новой строки, так проще читать код:
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'description'
+    ).filter(
+        (Q(is_on_main=True) & Q(is_published=True))
+        | Q(title__contains='пломбир') & Q(is_published=True)
+    )
+    # Полученный из БД QuerySet передаём в словарь контекста:
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    # Словарь контекста передаём в шаблон, рендерим HTML-страницу:
+    return render(request, template_name, context)
